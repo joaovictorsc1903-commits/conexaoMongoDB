fdc9f9083b9ab50df1be49f360aed5d365391f4b
Victor <joaovictorsc1903@gmail.com>
2025-11-03 15:42:25 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// 1. Import as bibliotecas
+// 1. Import as bibliotecas 
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 // 2. configurar o servidor Express
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(cors()); //Permite que o front end acesse este back-end
+app.use(express.json()); // Permite que o servidor entenda JSON
 
 // 3. conectar ao mongoDb
-const MongoURI = '';
-mongoose.connect(MongoURI, { useNewParser: true, useUnifieldTopology: true })
+const MongoURI = 'mongodb://localhost:27017/relatorio_atividades'; //colocar a url do seu projeto no mongoDB
+mongoose.connect(MongoURI, { useNewUrlParser: true, useUnifiedTopology: true }) // corrigi aqui
     .then(() => console.log('Conectado ao MongoDB com sucesso!'))
     .catch(err => console.error('Erro ao conectar ao MongoDB: ', err));
 
@@ -38,10 +38,10 @@ const relatorioSchema = new mongoose.Schema({
 // 5. Criar o "Model" - O objeto que representa sua coleção no banco
 const Relatorio = mongoose.model('Relatorio', relatorioSchema);
 
-// 6. Criar a "Rota" ou "Endpoint" - o URL que o front-end irá chamar
-app.post('./salvar-relatorio', async(req, res) =>{
+// 6.Criar a "Rota" ou "Endpoint" - o URL que o front-end irá chamar
+app.post('/salvar-relatorio', async(req, res) =>{ //arrumei aqui
     try{
-        //Pega-os dados que o front-end enviou (estão em req.body)
+        //Pega os dados que o front-end enviou (estão em req.body)
         const dadosDoFormulario = req.body;
 
         // Cria um novo documento com base nos dados
@@ -50,13 +50,13 @@ app.post('./salvar-relatorio', async(req, res) =>{
         // Salva o documento no banco de dados
         await novoRelatorio.save();
 
-        // Envia uma resposta com sucesso de volta para o front-end
-        res.status(201).json({message: 'Relatório salvo com sucesso!'})
+        // Envia uma resposta de sucesso de volta para o front-end
+        res.status(201).json({message: 'Relatório salvo com sucesso!'});
         console.log('Relatório salvo: ', novoRelatorio.titulo);
     }catch(error){
-        // se der erro, envia uam mensagem de erro
-        res.status(500).json({message: 'Ocoreu um erro ao salvar o relatório.', error: error});
-        console.error('Erro ao salvar: ',error);
+        // se der erro, envia uma mensagem de erro
+        res.status(500).json({ message: 'Ocorreu um erro ao salvar o relatório.', error: error});
+        console.error('Erro ao salvar: ', error);
     }
 });
 
